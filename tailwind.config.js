1e39f1df841bdd8cc8c6025227b4bee1b3bbfbd5
atlas <a.sproson@outlook.com>
2022-06-03 13:37:13 +0100

    Adds screen sizes and colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1448px'
+    },
+    extend: {
+      colors: {
+        yellowLight: '#fbffa7',
+        blueLight: '#b1c5ff',
+        greenLight: '#b6ffc0',
+        orange: '#ff764d',
+        lavender: '#d5b3ff',
+        blue: '#0000ff'
+      }
+    },
   },
   plugins: [],
 }
