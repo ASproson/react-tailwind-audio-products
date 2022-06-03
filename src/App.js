d71052144cadeafdd0f82d4e1a9c131a81bb09ab
atlas <a.sproson@outlook.com>
2022-06-03 22:16:55 +0100

    Adds base SubSectionOne component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
+import SubSectionOne from "./components/SubSectionOne";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <HeroSection />
+      <SubSectionOne />
     </div>
   );
 }
