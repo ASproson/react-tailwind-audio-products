ef73283e0ac3f5ee2dacc062fc0e9cfeff31c38f
atlas <a.sproson@outlook.com>
2022-06-24 18:49:41 +0100

    Resolves React error re class/Name

--- src/components/SubSectionOne.jsx
+++ src/components/SubSectionOne.jsx
@@ -7,7 +7,7 @@ const SubSectionOne = () => {
   const rightImage = "https://images.unsplash.com/photo-1583568671741-c75e1e2e4389?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
 
   return (
-    <div class="mt-20 mb-20 flex justify-center items-center">
+    <div className="mt-20 mb-20 flex justify-center items-center">
       <div id="yellow-container" className="ml-[850px] mr-96 flex absolute bg-yellowLight w-3/6 z-0 h-2/4  justify-center items-center">
         <div id="yellow-container-image">
           <div className="absolute inset-0 flex justify-center items-center z-10 ml-96 ">
